989cee28f5fbfc3ad3d6e924e742d5f73a2f0ea3
João Lucas S. A. Maia <130009108+Joaosamaia@users.noreply.github.com>
2023-12-14 21:15:08 -0300

    Update Produto.java

--- classes/Produto.java
+++ classes/Produto.java
@@ -1,32 +1,23 @@
 package classes;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-//import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public abstract class Produto{
 	@Id
-	@GeneratedValue(generator = "custom-generator")
-   	@GenericGenerator(name = "custom-generator", strategy = "com.project.CustomIdGenerator")
 	private String produtoID;
-	@Column(name="NOME")
 	private String nome;
-	@Column(name="PRECO")
 	private Double preco;
-	@Column(name="PORÇAO")
 	private Double porcão;
 
 	@ManyToOne
 	private Fornecedor fornecedor;
-
-	//ManyToOne estoque
-	//ManyToOne produto
+	@ManyToOne 
+	private Estoque estoque;
+	@ManyToOne 
+	private Pedido pedido;
 
 	//constructors
 	public Produto() {
@@ -34,13 +25,18 @@ public abstract class Produto{
 	public Produto(String produtoID) {
 		this.produtoID = produtoID;
 	}
-	public Produto(String produtoID, String nome, Double preco, Fornecedor fornecedor) {
+	public Produto(String produtoID, String nome, Double preco, Double porcão, Fornecedor fornecedor, Estoque estoque,
+			Pedido pedido) {
+		super();
 		this.produtoID = produtoID;
 		this.nome = nome;
 		this.preco = preco;
+		this.porcão = porcão;
 		this.fornecedor = fornecedor;
+		this.estoque = estoque;
+		this.pedido = pedido;
 	}
-
+	
 	//getters and setters:
 
 	public String getProdutoID() {
@@ -63,19 +59,30 @@ public abstract class Produto{
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
-	public Fornecedor getFornecedor() {
-		return fornecedor;
-	}
-	public void setFornecedor(Fornecedor fornecedor) {
-		this.fornecedor = fornecedor;
-	}
-
 	public Double getPorcão() {
 		return porcão;
 	}
 	public void setPorcão(Double porcão) {
 		this.porcão = porcão;
 	}
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	public Estoque getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(Estoque estoque) {
+		this.estoque = estoque;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	
 
 }
