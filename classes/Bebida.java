10c2eb04bf11cf6bd72b92ab9ddcc4c50816a6d2
João Lucas S. A. Maia <130009108+Joaosamaia@users.noreply.github.com>
2023-12-14 21:19:20 -0300

    Update Bebida.java

--- classes/Bebida.java
+++ classes/Bebida.java
@@ -4,18 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Bebida extends Produto{
-	//add bebida specific atributes
-	@Id
+public class Comida extends Produto{
+	//add comida specific atributes
 	private String tipo;
 	private String ingredientes;
-	private String temperatura;
 	private String descricao;
-
+	
 	//create constructors
 
 
-	//implement methods for Bebida
+	//implement methods for Comida
 	void imprimir(String a) {
 	}
 	
