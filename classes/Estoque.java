aaf42d8308947b36f0272980b1b9e85210742288
João Lucas S. A. Maia <130009108+Joaosamaia@users.noreply.github.com>
2023-12-14 21:17:58 -0300

    Update Estoque.java

--- classes/Estoque.java
+++ classes/Estoque.java
@@ -1,16 +1,52 @@
 package classes;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 @Entity
 public class Estoque {
     @Id
-	@GeneratedValue(generator = "custom-generator")
-   	@GenericGenerator(name = "custom-generator", strategy = "com.project.CustomIdGenerator")
 	private String estoqueID;
     
-    //OneToMany produto
-    //ManyToMany pedido
-    //ManyToMany fornecedor
-
+    @OneToMany(mappedBy="estoque", fetch = FetchType.LAZY)
+    private List<Produto> produto;
+    @ManyToMany 
+    private List<Pedido> pedido;
+    @ManyToMany 
+    private List<Fornecedor> fornecedor;
+    
+	//getters and setters:
+    
+	public String getEstoqueID() {
+		return estoqueID;
+	}
+	public void setEstoqueID(String estoqueID) {
+		this.estoqueID = estoqueID;
+	}
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	public List<Pedido> getPedido() {
+		return pedido;
+	}
+	public void setPedido(List<Pedido> pedido) {
+		this.pedido = pedido;
+	}
+	public List<Fornecedor> getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(List<Fornecedor> fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+    
     	//add functions for stock management, venda, compra
 		/*such as:
 	AddStockItem:
